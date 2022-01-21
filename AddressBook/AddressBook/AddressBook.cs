07536fb182cabbfc99cb2ea9b6704e488662c3e2
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-21 11:25:34 +0530

    [Raja] Added address book

---
+++ AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    /// <summary>
+    /// Address Book
+    /// </summary>
+    internal class AddressBook
+    {
+        /// <summary>
+        /// Displaying the Welcome message 
+        /// </summary>
+        public static void DispalyUC1Address() //creating moethod for a class
+        {
+            Console.WriteLine("Welcome to the Address Book Program ");
+        }
+    }
+}
