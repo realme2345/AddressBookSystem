32e8509782b4f231f70899ff58a62466d93c4337
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-22 21:01:21 +0530

    [Raja] Added UC5AddreeBook implimented in AddressBook

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -10,23 +10,15 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please enter a command : 1=add,2=remove,3=list");
-
-            int num =Convert.ToInt32(Console .ReadLine()); 
-            
-                switch(num)
-                {
-                    case 1:
-                    AddressBook.AddPerson();
-                    break;
-                    case 2:
-                    AddressBook.RemovePerson();
-                    break ;
-                    case 3:
-                    AddressBook.ListPeople();
-                    break ;
-                }
-            Console.Read();
+            Console.WriteLine(" Add Person :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            while (x <= num)
+            {
+                AddressBook.AddPerson();
+                x++;
+            }
+            AddressBook.ListPeople();
         }
     }
 }
