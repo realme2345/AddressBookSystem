f8514b6401932e48852a5347db7479c47862ce5c
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-09 22:55:26 +0530

    [Raja]Add to search the people by using thier city names implemented in AddressBook

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -11,74 +11,23 @@ namespace AddressBook
     /// </summary>
     class AddressBook
     {
-        private string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string State { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
-        public string Addresses { get; set; }
-
-        public static Dictionary<string, AddressBook> People = new Dictionary<string, AddressBook>();
-        public static Dictionary<string, AddressBook> AllPeople = new Dictionary<string, AddressBook>();
-        //creating Dictionaries for to store the all address of people
-        public static void DublicatePerson()//It is used to Add Number Address avoiding dublicate persons based on thier names
+        public string FirstName;
+        public string LastName;
+        public string PhoneNumber;
+        public string State;
+        public string City;
+        public string ZipCode;
+        public string Addresses;
+        //Creating a constructer
+        public AddressBook(string firstName, string lastName, string phoneNumber, string state, string city, string zipCode, string addresses)
         {
-            foreach (var Element in AllPeople)
-            {
-               
-                    if (!AllPeople.ContainsKey(Element.Key))
-                    throw new CustomException(CustomException.ExceptionType.PERSON_ALREADY_EXISTS, "Already Exists");
-               
-            }
-            AddPerson();
-        }
-        public static void AddPerson()  //creating metod for a class
-        {
-            AddressBook person = new AddressBook(); // creating a object for to access instance method or variable
-            Console.Write("Enter First Name: "); // Taking the inputs from user
-            person.FirstName = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            person.LastName = Console.ReadLine();
-            Console.Write("Enter Phone Number: ");
-            person.PhoneNumber = Console.ReadLine();
-            Console.Write("Enter Address 1: ");
-            person.Addresses = Console.ReadLine();
-            Console.WriteLine("Enter State");
-            person.State = Console.ReadLine();
-            Console.WriteLine("Enter a City");
-            person.City = Console.ReadLine();
-            Console.WriteLine("Enter the Zip Code");
-            person.ZipCode = Console.ReadLine();
-            People.Add(person.FirstName, person);// adding the peoples to the Dictionaries
-        }
-        private static void PrintPerson(AddressBook person)
-        {
-            Console.WriteLine("First Name: " + person.FirstName);
-            Console.WriteLine("Last Name: " + person.LastName);
-            Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("Address : " + person.Addresses);
-            Console.WriteLine("State : " + person.State);
-            Console.WriteLine("City : " + person.City);
-            Console.WriteLine("ZipCode :" + person.ZipCode);
-            Console.WriteLine("-------------------------------------------");
-        }
-        public static void ListPeople() //This method used to show people who are present in Address book
-        {
-            if (People.Count == 0)
-            {
-                Console.WriteLine("Your address book is empty. Press any key to continue.");
-                Console.ReadKey();
-                return;
-            }
-            Console.WriteLine("Here are the current people in your address book:\n");
-            foreach (var Element in People.Values)
-            {
-                PrintPerson(Element);
-            }
-
-            Console.WriteLine("\nPress any key to continue.");
-            Console.ReadKey();
+            this.FirstName = firstName; //Assigning data
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.State = state;
+            this.City = city;
+            this.ZipCode = zipCode;
+            this.Addresses = addresses;
         }
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -8,28 +8,77 @@ namespace AddressBook
 {
     internal class Program
     {/// <summary>
-    /// Employee Wage
-    /// </summary>
-    /// <param name="args"></param>
+     /// Employee Wage
+     /// </summary>
+     /// <param name="args"></param>
+        public static List<AddressBook> list = new List<AddressBook>();
         static void Main(string[] args) //Ability to add multiple person to address book and stored in Dictionaries
         {
-            Console.WriteLine("Enter a Number of Address to Add :");
-            int n=Convert.ToInt32(Console.ReadLine());
-            AddressBook.AddPerson();
-            int i = 1;
-            while (i <= n)
+            AddPerson();
+            //AddPerson();
+            ListPeople();
+            SearchPerson();
+        }
+        public static void AddPerson()//method for adding multiple members
+        {
+            list.Add(new AddressBook("Raja",
+                                     "Kongara",
+                                     "9133139939",
+                                     "Telanagana",
+                                     "HYD",
+                                     "507143",
+                                       "ASDR"));
+            list.Add(new AddressBook("Nari",
+                                     "Gaggilla",
+                                     "9133139939",
+                                     "Telanagana",
+                                     "VJW",
+                                     "507022",
+                                       "ASD"));
+            list.Add(new AddressBook("Nagi", "Gajjella", "89826336522", "Telanagan", "Khamma", "507122", "shdhd"));
+        }
+
+        public static void PrintPerson(AddressBook person)//this method to print the persons
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Address : " + person.Addresses);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("City : " + person.City);
+            Console.WriteLine("ZipCode :" + person.ZipCode);
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void ListPeople() //This method used to show people who are present in Address book
+        {
+            if (list.Count == 0)
             {
-                try
-                {
-                    AddressBook.DublicatePerson();//this method for avoid dublicate persons in address book
-                    i++;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
             }
-            AddressBook.ListPeople();
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var Element in list)
+            {
+                PrintPerson(Element);
+            }
+        }
+        public static void SearchPerson() //this method for search the people with respect their names
+        {
+            Console.WriteLine("Enter the City name of the person you would like to Know:");
+            string city = Console.ReadLine();
+            AddressBook person = list.FirstOrDefault(x => x.City.ToLower() == city.ToLower());
+            while (person != null)
+            {
+                Console.WriteLine("Person is present succefull");
+                PrintPerson(person);
+                Console.ReadKey();
+                return;
+            }
+            if (person == null)
+            {
+                Console.WriteLine("Person is not present");
+            }
         }
     }
 }
