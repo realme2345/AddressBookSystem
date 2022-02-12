02efe66c5bb0af953ea996d3bf9a52b921b962e7
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-12 18:40:11 +0530

    [Raja]Add Read the file using the File IO implemented AddressBook

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -29,6 +29,71 @@ namespace AddressBook
             this.ZipCode = zipCode;
             this.Addresses = addresses;
         }
+        public static List<AddressBook> list = new List<AddressBook>();
+        public static void AddPerson()//method for adding multiple members
+        {
+            list.Add(new AddressBook("Raja",
+                                     "Kongara",
+                                     "9133139939",
+                                     "Andhra",
+                                     "HYD",
+                                     "507143",
+                                       "ASDR"));
+            list.Add(new AddressBook("Nari",
+                                     "Gaggilla",
+                                     "9133139939",
+                                     "Tamilnaadu",
+                                     "HYD",
+                                     "507022",
+                                       "ASD"));
+            list.Add(new AddressBook("Nagi", "Gajjella", "89826336522", "Telanagan", "Khamma", "507122", "shdhd"));
+        }
+
+        public static void PrintPerson(AddressBook person)//this method to print the persons
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Address : " + person.Addresses);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("City : " + person.City);
+            Console.WriteLine("ZipCode :" + person.ZipCode);
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void ListPeople() //This method used to show people who are present in Address book
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var Element in list)
+            {
+                PrintPerson(Element);
+            }
+        }
+        public static void SortedBasedOnCity() //this method for sorted the persons based on their city
+        {
+            Console.WriteLine("Sorted by thier City");
+            Console.WriteLine("-------------------------------");
+            var person = list.OrderByDescending(x => x.City);
+            foreach (var Element in person)
+            {
+                PrintPerson(Element);
+            }
+        }
+        public static void SortedBasedOnState() //this method for sorted the persons based on their state
+        {
+            Console.WriteLine("Sorted by thier state");
+            Console.WriteLine("-------------------------------");
+            var person = list.OrderBy(x => x.State);
+            foreach (var Element in person)
+            {
+                PrintPerson(Element);
+            }
+        }
     }
 }
    
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,78 +12,17 @@ namespace AddressBook
      ///Address Book
      /// </summary>
      /// <param name="args"></param>
-        public static List<AddressBook> list = new List<AddressBook>();
         static void Main(string[] args) //Ability to add multiple person to address book and stored in Dictionaries
         {
-            AddPerson();
-            //AddPerson();
-            ListPeople();
-            SortedBasedOnCity(list);
-            SortedBasedOnState(list);
-        }
-        public static void AddPerson()//method for adding multiple members
-        {
-            list.Add(new AddressBook("Raja",
-                                     "Kongara",
-                                     "9133139939",
-                                     "Andhra",
-                                     "HYD",
-                                     "507143",
-                                       "ASDR"));
-            list.Add(new AddressBook("Nari",
-                                     "Gaggilla",
-                                     "9133139939",
-                                     "Tamilnaadu",
-                                     "HYD",
-                                     "507022",
-                                       "ASD"));
-            list.Add(new AddressBook("Nagi", "Gajjella", "89826336522", "Telanagan", "Khamma", "507122", "shdhd"));
-        }
-
-        public static void PrintPerson(AddressBook person)//this method to print the persons
-        {
-            Console.WriteLine("First Name: " + person.FirstName);
-            Console.WriteLine("Last Name: " + person.LastName);
-            Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("Address : " + person.Addresses);
-            Console.WriteLine("State : " + person.State);
-            Console.WriteLine("City : " + person.City);
-            Console.WriteLine("ZipCode :" + person.ZipCode);
-            Console.WriteLine("-------------------------------------------");
-        }
-        public static void ListPeople() //This method used to show people who are present in Address book
-        {
-            if (list.Count == 0)
-            {
-                Console.WriteLine("Your address book is empty. Press any key to continue.");
-                Console.ReadKey();
-                return;
-            }
-            Console.WriteLine("Here are the current people in your address book:\n");
-            foreach (var Element in list)
-            {
-                PrintPerson(Element);
-            }
-        }
-        public static void SortedBasedOnCity(List<AddressBook>list) //this method for sorted the persons based on their city
-        {
-            Console.WriteLine("Sorted by thier City");
-            Console.WriteLine("-------------------------------");
-            var person= list.OrderByDescending(x => x.City);
-            foreach (var Element in person)
-            {
-                 PrintPerson(Element);
-            }
-        }
-        public static void SortedBasedOnState(List<AddressBook> list) //this method for sorted the persons based on their state
-        {
-            Console.WriteLine("Sorted by thier state");
-            Console.WriteLine("-------------------------------");
-            var person = list.OrderBy(x => x.State);
-            foreach (var Element in person)
-            {
-                PrintPerson(Element);
-            }
+            //AddressBook.AddPerson();
+            //AddressBook.ListPeople();
+            //AddressBook.SortedBasedOnCity();
+            //AddressBook.SortedBasedOnState();
+            // this method for Reading the Data from The File
+            string path = @"D:\git\AddressBook\AddressBookSystem\AddressBook\AddressBook\AddressBook.cs";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
         }
     }
 }
